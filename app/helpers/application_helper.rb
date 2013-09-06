df8f01119f90b71176effe19fa320bbbbc97fe6d
Marco De Canal <marco.decanal@gmail.com>
2013-09-06 11:57:48 +0200

    Added stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	#Fornisce un titolo in base alla pagina richiesta
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
